5effc0237a73830ae7fda3810747b3c94f5f5ba2
vikashchaurasia <vikashchaurasia@Mac-Pro.local>
2023-10-09 15:14:41 +0900

    data ingestion done

---
+++ templates/main.py
@@ -0,0 +1,15 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.state_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = " Data Ingestion Stage"
+
+try:
+        
+        logger.info(f">>>> stage {STAGE_NAME} started>>>>")
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>> stage {STAGE_NAME} completed>>>>")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
